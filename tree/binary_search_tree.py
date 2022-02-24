fca9e466b11fa45cf6428113cd794b1905face52
鍾雲竹 <lucien.yc.chung@mail.foxconn.com>
2022-02-24 18:03:49 +0800

    update lookup func

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -1,4 +1,4 @@
-from threading import currentThread
+import json
 
 
 class Node:
@@ -6,6 +6,9 @@ class Node:
         self.left = None
         self.right = None
         self.value = value
+
+    def __repr__(self) -> str:
+        return str({'value': self.value, 'right': self.right, 'left': self.left})
 
 class BinarySearchTree:
     def __init__(self) -> None:
@@ -20,47 +23,66 @@ class BinarySearchTree:
                 return node.left
         return None
                 
-
-
     def insert(self, value):
         new_node = Node(value)
         if not self.root:
-            self.root = Node(value)
-        
-        if value > self.root.value:
-            self.root.right
-        
-        current = self.root
-        next = self.next(current, value)
-        while next is not None:
-            current = next
-            next = self.next(current, value)
-        
-        if value > current.value:
-            current.right = new_node
-        elif value < current.value:
-            current.left = new_node
-        else:
-            return 
+            self.root = new_node
+            return self
 
+        current_node = self.root
+        while True:
+            if value >= current_node.value:
+                if current_node.right is None:
+                    current_node.right = new_node
+                    return self
+                current_node = current_node.right
+            
+            else:
+                if current_node.left is None:
+                    current_node.left = new_node
+                    return self
+                current_node = current_node.left
+                    
     def lookup(self, value):
+        if self.root is None:
+            return None
+        current_node = self.root
+        while True:
+            if value == current_node.value:
+                return current_node
+            elif value > current_node.value:
+                if current_node.right is None:
+                    return None
+                current_node = current_node.right
+            elif value < current_node.value:
+                if current_node.left is None:
+                    return None
+                current_node = current_node.left
+    
+    def remove(self, value):
         pass
+
+                
+
 
 
 
 if __name__ == "__main__":
     tree = BinarySearchTree()
     tree.insert(9)
+    # tree.insert(9)
     tree.insert(4)
     tree.insert(6)
     tree.insert(20)
     tree.insert(170)
     tree.insert(15)
     tree.insert(1)
-    print(tree.root.value)
-    print(tree.root.left.value)
-    print(tree.root.left.right.value)
-    print(tree.root.left.left.value)
-    print(tree.root.right.value)
-    print(tree.root.right.right.value)
-    print(tree.root.right.left.value)
+    print(tree.lookup(9))
+    # print(tree.root.value)
+    # print(tree.root.left.value)
+    # print(tree.root.left.right.value)
+    # print(tree.root.left.left.value)
+    # print(tree.root.right.value)
+    # print(tree.root.right.right.value)
+    # print(tree.root.right.left.value)
+    # print(tree.root.right.left.left.value)
